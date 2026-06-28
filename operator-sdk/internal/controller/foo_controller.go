f177ccd8af6dc1be2cc8cc754229e40849a11dba
takuteh <takumi.ktym@gmail.com>
2026-06-28 21:53:21 +0900

    operator-sdk版のRBACを修正

--- operator-sdk/internal/controller/foo_controller.go
+++ operator-sdk/internal/controller/foo_controller.go
@@ -49,6 +49,10 @@ type FooReconciler struct {
 // +kubebuilder:rbac:groups=samplecontroller.samplecontroller.example.com,resources=foos/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=samplecontroller.samplecontroller.example.com,resources=foos/finalizers,verbs=update
 
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get
+// +kubebuilder:rbac:groups=apps,resources=deployments/finalizers,verbs=update
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
